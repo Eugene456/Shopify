da97028540d31d2d30c1acf5a043f3cb700814c2
Eugenii Shevchenko <eugene9451@gmail.com>
2025-12-11 13:33:50 +0200

    Change mobile footer

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,14 @@ $(document).ready(function() {
     $('.burger-min').click(function(){
         $('.mobile-menu').fadeOut(300);
     });
+
+    $('.footer-title').each(function(i, item){
+        $(item).click(function(e){
+            e.preventDefault();
+            $(this).toggleClass('active');
+            $($('.footer__links')[i]).slideToggle(300);
+        });
+    });
+    
+    
 });
